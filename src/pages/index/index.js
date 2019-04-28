781f583e1b34672a49d602c7be0218b7e536b129
李高航 <gaohang.li@proudkids.cn>
2019-04-28 18:47:51 +0800

    数据请求封装/引入infinitescroll组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,17 +5,49 @@ import url from 'js/api.js'
 import 'css/common.css'
 import './index.css'
 
+// mint-UI
+import {InfiniteScroll} from 'mint-ui'
+Vue.use(InfiniteScroll)
+
+import customFooter from 'components/custom-footer.vue'
+
 new Vue({
   el: '#app',
+  components: {
+    customFooter
+  },
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 10,
+    notLoading: false,
+    allLoaded: false
   },
   created() {
-    axios.post(url.hotLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then(res => {
-      this.lists = res.data.lists;
-    })
+    this.getLists()
+  },
+  methods: {
+    getLists() {
+      if (this.allLoaded) return
+      this.notLoading = true
+      axios.post(url.hotLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        let curLists = res.data.data.lists
+        // 判断所有数组是否加载完毕
+        if (curLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if (!this.lists) {
+          //初次请求
+          this.lists = curLists
+        } else {
+          this.lists = this.lists.concat(curLists)
+        }
+        this.notLoading = false
+        this.pageNum++
+      })
+    }
   }
-});
+})

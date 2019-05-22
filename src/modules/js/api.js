dc65a08428b94c1aaa4b75d71c50779f9e6e21e5
李高航 <gaohang.li@proudkids.cn>
2019-05-22 18:27:58 +0800

    2019.0522.1829 macmini 同步进度

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,6 +1,9 @@
 let url = {
   hotLists: '/index/hotLists',
-  indexBanner: '/index/banner'
+  indexBanner: '/index/banner',
+  topList: '/category/topList',
+  subList: '/category/subList',
+  rank: '/category/rank'
 };
 
 let baseUrl = 'https://www.easy-mock.com/mock/5cbedc472b8e596c3bd450c4/youzan';
--- src/pages/category/catagory.js
+++ src/pages/category/catagory.js
@@ -5,3 +5,53 @@ import url from 'js/api.js'
 import 'css/common.css'
 import './catagory.css'
 
+import CustomFooter from 'components/CustomFooter.vue'
+
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            topList: null,
+            topIndex: 0,
+            subData: null,
+            rankata: null
+        }
+    },
+    methods: {
+        getTopList() {
+            axios.get(url.topList).then(res => {
+                this.topList = res.data.data.lists
+            }).catch(err => {
+                console.info(err)
+            })
+        },
+        getSubList(index, id) {
+            this.topIndex = index
+            //默认请求综合排行
+            if (index === 0) {
+                this.getRank()
+            } else {
+                //点击其他类后请求
+                axios.get(url.subList, {id}).then(res => {
+                    this.subData = res.data.data.data
+                }).catch(err => {
+                    console.info(err)
+                })
+            }
+        },
+        getRank() {
+            axios.get(url.rank).then(res => {
+                this.rankata = res.data.data.data
+            }).catch(err => {
+                console.info(err)
+            })
+        }
+    },
+    created() {
+        this.getTopList()
+        this.getSubList(0)
+    },
+    components: {
+        CustomFooter
+    }
+})
